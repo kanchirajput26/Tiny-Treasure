9da878e768db21653614e899346ebc049f47eb5b
kanchirajput26 <kanchirajput26@gmail.com>
2024-04-10 12:33:56 +0530

    Ecommerce shopping website

---
+++ src/component/category.js
@@ -0,0 +1,64 @@
+import { React } from "react";
+
+const divStyle = {
+  border: "None",
+  backgroundColor: "#0000000D",
+  borderRadius: "50%",
+  width: "100px",
+  height: "100px",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+  marginRight: "5px",
+  marginLeft:"10px" 
+};
+
+export default function Category() {
+  return (
+    <div style={{ marginLeft: 0, display: "flex", flexDirection: "row" }}>
+      {" "}
+      <div style={divStyle}>
+        <img
+          src="https://s3-alpha-sig.figma.com/img/e2fb/a2f9/4863952f90cee55434e088d76bd2fe7c?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=BVOWGD9n6x-cwLyUSjDPSGNYGmVlA8ui5oWC01R2oXOdVg12EeEhOVmFaa89XSr40IilOHRhikoBiQ3TCP~kvXB2qO68ogXoR9RlJ1PJTWj0jHksSiTaDAJEnE8lFUC~BG1bPdw7Rauev9cRQdEf7QZvsZDO-C0jTkTr3uOFDJ4CLMxvdNlytOl0UCxlhzCCzM~BIFNCWId60OFF~zzK2eI4QBxi~glvVm8nTPb-ZWgZbj8asJ7N~RQ4c9H1Y5Rm3sNb4cSyyZeb5LNjBRwGXFZpZAnem4u~s6TMfjA-qD1iHknuidtty6mvQIADpGHzta0ydL0cUFXIrXwQNh-s2w__"
+          alt="category"
+          style={{
+            marginTop: 25,
+            width: "50px",
+            height: "50px",
+          }}
+        />
+      </div>
+      <div style={divStyle}>
+        <img
+          src="https://s3-alpha-sig.figma.com/img/c21f/a5c1/1b4e8966ff6415613f50abfdbfa3ecb4?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=o1cvzpM1f4WiG9Up5nNVLNq-VkgcrsRJm3hyM8OjDaz-cdwsS7khaxqAljthH~brqRGlaLFfPbGpS9GlotYHlsVGu4ddd0v9F7S5jplXH-NBNmLWniUneAPSdP6bNBH~cw5JkdnLuDUzfBzvjMo4Tc41-5TT5C93NzDnzcjSX1ncnfKaqa57VLrKGj2N816sbBH4csohnLbgG0pQW9Jgol7AVkq89S9UREK8V7WXZSCE-KVCCydvnUIVzvJFrDj~LIFwA00LrIMX-jw0gyKeAUKqy0VnndO63rUtHhlhYO7BWKUHCL0~DnwAuuJi--Wvp97TAYk6D4UZOOHxyZEDQw__"
+          alt="category"
+          style={{
+            marginTop: 25,
+            width: "50px",
+            height: "50px",
+          }}
+        />
+      </div>
+      <div style={divStyle}>
+          <img src="https://s3-alpha-sig.figma.com/img/c21f/a5c1/1b4e8966ff6415613f50abfdbfa3ecb4?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=o1cvzpM1f4WiG9Up5nNVLNq-VkgcrsRJm3hyM8OjDaz-cdwsS7khaxqAljthH~brqRGlaLFfPbGpS9GlotYHlsVGu4ddd0v9F7S5jplXH-NBNmLWniUneAPSdP6bNBH~cw5JkdnLuDUzfBzvjMo4Tc41-5TT5C93NzDnzcjSX1ncnfKaqa57VLrKGj2N816sbBH4csohnLbgG0pQW9Jgol7AVkq89S9UREK8V7WXZSCE-KVCCydvnUIVzvJFrDj~LIFwA00LrIMX-jw0gyKeAUKqy0VnndO63rUtHhlhYO7BWKUHCL0~DnwAuuJi--Wvp97TAYk6D4UZOOHxyZEDQw__"
+          alt="category"
+          style={{
+            marginTop: 25,
+            width: "50px",
+            height: "50px",
+          }}
+        />
+      </div>
+      <div style={divStyle}>
+          <img src="https://s3-alpha-sig.figma.com/img/c21f/a5c1/1b4e8966ff6415613f50abfdbfa3ecb4?Expires=1713139200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=o1cvzpM1f4WiG9Up5nNVLNq-VkgcrsRJm3hyM8OjDaz-cdwsS7khaxqAljthH~brqRGlaLFfPbGpS9GlotYHlsVGu4ddd0v9F7S5jplXH-NBNmLWniUneAPSdP6bNBH~cw5JkdnLuDUzfBzvjMo4Tc41-5TT5C93NzDnzcjSX1ncnfKaqa57VLrKGj2N816sbBH4csohnLbgG0pQW9Jgol7AVkq89S9UREK8V7WXZSCE-KVCCydvnUIVzvJFrDj~LIFwA00LrIMX-jw0gyKeAUKqy0VnndO63rUtHhlhYO7BWKUHCL0~DnwAuuJi--Wvp97TAYk6D4UZOOHxyZEDQw__"
+          alt="category"
+          style={{
+            marginTop: 25,
+            width: "50px",
+            height: "50px",
+          }}
+        />
+      </div>
+     
+    </div>
+  );
+}
---
+++ src/component/categoryElement.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Image } from "antd";
+const divStyle = {
+  border: "None",
+  backgroundColor: "#0000000D",
+  borderRadius: "50%",
+  width: "100px",
+  height: "100px",
+  backgroundSize: "cover",
+  backgroundPosition: "center",
+};
+
+function CategoryElement() {
+  <div style={divStyle}>
+    <img
+      src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+      alt="category"
+      style={{ marginTop:50, width: "100px", height: "100px" }}
+    />
+    ;
+  </div>;
+}
+
+export default CategoryElement;
